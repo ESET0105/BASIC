82423c4a5c243b06bb68bfc16197f56fd8adf639
MILANROAT018 <roatmilan@gmail.com>
2025-09-23 15:41:10 +0530

    added .sh file in HDFS and updated react_event folder

---
+++ HDFS/user_data.sh
@@ -0,0 +1,14 @@
+#! usr/bin/bash
+
+
+echo "please write your name"
+
+read name
+
+
+echo "plase enter your age"
+read age
+
+echo "Congratulations , you name from now is " $name "and your age has become" $age
+
+ 
